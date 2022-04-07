c8de44c85cd843c5d839c5903cb28b0188ee440d
0seo8 <00seo23@gmail.com>
2022-04-07 15:39:58 +0900

    feat: Add axios API

--- Javascript/News/App.js
+++ Javascript/News/App.js
@@ -1,1 +1,13 @@
 // do something!
+
+import axios from 'axios'
+
+function fetchNews () {
+  axios
+  .get('https://newsapi.org/v2/top-headlines?country=us&apiKey=d417d50a085b4ed4a42bae22070aa8b3')
+  .then((response) => {
+    console.log(response)
+  })
+}
+
+fetchNews()
