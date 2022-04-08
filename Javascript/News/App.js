2b6ad3146150cbc79fedab96f9488433fe5ce5fd
0seo8 <00seo23@gmail.com>
2022-04-08 12:56:29 +0900

    feat: create Nav area

--- Javascript/News/App.js
+++ Javascript/News/App.js
@@ -1,17 +1,6 @@
-// do something!
+import {render} from './components/Nav.js'
 
-import './style.css'
-import axios from 'axios'
+const $root = document.querySelector('#root');
 
-// pageSize에 5를 지정하면 5개의 뉴스를 취득한다.
-const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${category === 'all' ? '' : category}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`
+render($root)
 
-function fetchNews () {
-  axios
-  .get(url)
-  .then((response) => {
-    console.log(response)
-  })
-}
-
-fetchNews()
--- Javascript/News/components/Nav.js
+++ Javascript/News/components/Nav.js
@@ -1,1 +1,30 @@
-// do something!
+export let state = {
+  items: [
+  ['all', '전체보기'],
+  ['business', '비즈니스'],
+  ['entertainment', '엔터테인먼트'],
+  ['health', '건강'],
+  ['science', '과학'],
+  ['sports', '스포츠'],
+  ['technology', '기술'],
+]};
+
+export const render = ($root) => {
+  const { items } = state;
+
+  $root.innerHTML = `
+    <ul>
+      ${items.map(item => `<li id=${item[0]} class=${item[1]}>${item[1]}</li>`).join('')}
+    </ul>
+  `;
+  document.querySelector('#append').addEventListener('click', () => {
+    setState({ items: [ ...items, `item${items.length + 1}` ] })
+  })
+}
+
+const setState = (newState) => {
+  state = { ...state, ...newState };
+  render();
+}
+
+
